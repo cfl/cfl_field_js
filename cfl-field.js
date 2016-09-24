2a7cab300e57c9af1e15171d6f5a3f91f2905a02
Sully Syed <sully@yllus.com>
2016-09-24 19:30:39 -0400

    Rework variable names.

--- cfl-field.js
+++ cfl-field.js
@@ -1,18 +1,18 @@
-function Field( elementId, visitorTeamAbbreviation, homeTeamAbbreviation ) {
-	this.elementId = elementId;
+function Field( element_id, visitor_team_abbreviation, home_team_abbreviation ) {
+	this.element_id = element_id;
 	//this.currentPoint = 0;
 	//this.currentPlayY = 10;
 
 	// Define information about the teams involved.
-	this.visitorTeamAbbreviation = visitorTeamAbbreviation;
-	this.visitorTeamLetter = null;
-	this.visitorTeamColour = null;
-	this.homeTeamAbbreviation = homeTeamAbbreviation;
-	this.homeTeamLetter = null;
-	this.homeTeamColour = null;
+	this.visitor_team_abbreviation = visitor_team_abbreviation;
+	this.visitor_team_letter = null;
+	this.visitor_team_colour = null;
+	this.home_team_abbreviation = home_team_abbreviation;
+	this.home_team_letter = null;
+	this.home_team_colour = null;
 
 	// Define values that set how the field looks.
-	this.yardToPixelMultipler = document.getElementById(this.elementId).width / 150;
+	this.yardToPixelMultipler = document.getElementById(this.element_id).width / 150;
 	this.hashMarkSecondLineMultiplier = 0.36923076923077;
 	this.hashMarkThirdLineMultiplier = 0.63076923076923;
 	this.yardMarkersFirstLineMultiplier = 0.18461538461538;
@@ -21,20 +21,24 @@ function Field( elementId, visitorTeamAbbreviation, homeTeamAbbreviation ) {
 	this.yardLineBetweenWidth = 5;
 	this.hashMarkHeight = this.yardToPixelMultipler;
 	
-	this.ctx = document.getElementById(elementId).getContext('2d');
+	this.ctx = document.getElementById(element_id).getContext('2d');
 	//this.ctx.scale(2,2);
 
+	this.drawPlay = function( team_abbreviation, play_type_id, field_position_start, field_position_end ) {
+		1;
+	}
+
 	this.setTeamInfo = function() {
-		this.visitorTeamLetter = this.getTeamValue(this.visitorTeamAbbreviation, 'letter');
-		this.visitorTeamColour = this.getTeamValue(this.visitorTeamAbbreviation, 'colour');
-		this.homeTeamLetter = this.getTeamValue(this.homeTeamAbbreviation, 'letter');
-		this.homeTeamColour = this.getTeamValue(this.homeTeamAbbreviation, 'colour');
+		this.visitor_team_letter = this.getTeamValue(this.visitor_team_abbreviation, 'letter');
+		this.visitor_team_colour = this.getTeamValue(this.visitor_team_abbreviation, 'colour');
+		this.home_team_letter = this.getTeamValue(this.home_team_abbreviation, 'letter');
+		this.home_team_colour = this.getTeamValue(this.home_team_abbreviation, 'colour');
 	}
 	
-	this.draw = function() {
+	this.drawField = function() {
 		// Fill the inside of the field.
 		this.ctx.fillStyle = "rgb(0, 153, 41)";
-		this.ctx.fillRect(0, 0, document.getElementById(this.elementId).width, document.getElementById(this.elementId).height);
+		this.ctx.fillRect(0, 0, document.getElementById(this.element_id).width, document.getElementById(this.element_id).height);
 
 		// Fill the end zones.
 		this.fillEndZones();
@@ -63,7 +67,7 @@ function Field( elementId, visitorTeamAbbreviation, homeTeamAbbreviation ) {
 		// Draw the thicker endzone lines.
 		this.ctx.lineWidth = 2;
 		this.drawLine(int_width_of_endzone - 1);
-		this.drawLine(document.getElementById(this.elementId).width - int_width_of_endzone);
+		this.drawLine(document.getElementById(this.element_id).width - int_width_of_endzone);
 	}
 
 	this.createHashMarks = function() {
@@ -76,11 +80,11 @@ function Field( elementId, visitorTeamAbbreviation, homeTeamAbbreviation ) {
 		for ( var i = 0; i < 110; i++ ) {
 			this.drawHashes( (int_width_of_endzone + (i * this.yardToPixelMultipler)), 0 );
 
-			this.drawHashes( (int_width_of_endzone + (i * this.yardToPixelMultipler)), (document.getElementById(this.elementId).height * this.hashMarkSecondLineMultiplier) );
+			this.drawHashes( (int_width_of_endzone + (i * this.yardToPixelMultipler)), (document.getElementById(this.element_id).height * this.hashMarkSecondLineMultiplier) );
 
-			this.drawHashes( (int_width_of_endzone + (i * this.yardToPixelMultipler)), (document.getElementById(this.elementId).height * this.hashMarkThirdLineMultiplier) );
+			this.drawHashes( (int_width_of_endzone + (i * this.yardToPixelMultipler)), (document.getElementById(this.element_id).height * this.hashMarkThirdLineMultiplier) );
 
-			this.drawHashes( (int_width_of_endzone + (i * this.yardToPixelMultipler)), (document.getElementById(this.elementId).height - this.hashMarkHeight) );
+			this.drawHashes( (int_width_of_endzone + (i * this.yardToPixelMultipler)), (document.getElementById(this.element_id).height - this.hashMarkHeight) );
 		}
 	}
 
@@ -96,7 +100,7 @@ function Field( elementId, visitorTeamAbbreviation, homeTeamAbbreviation ) {
 			var str_yard_marker = this.getYardMarker(i);
 
 			this.ctx.save(); // Save the unrotated context of the canvas.
-			this.ctx.translate(int_width_of_endzone + (10 * i * this.yardToPixelMultipler), (document.getElementById(this.elementId).height * this.yardMarkersFirstLineMultiplier)); // Move to the center of the canvas to (x, y) point.
+			this.ctx.translate(int_width_of_endzone + (10 * i * this.yardToPixelMultipler), (document.getElementById(this.element_id).height * this.yardMarkersFirstLineMultiplier)); // Move to the center of the canvas to (x, y) point.
 			this.ctx.scale(-1, -1);
 			this.ctx.fillText(str_yard_marker, 0, 0);
 			this.ctx.restore(); // Restore the unrotated context.
@@ -106,14 +110,14 @@ function Field( elementId, visitorTeamAbbreviation, homeTeamAbbreviation ) {
 		for ( var i = 1; i < 11; i++ ) {
 			var str_yard_marker = this.getYardMarker(i);
 			
-			this.ctx.fillText(str_yard_marker, int_width_of_endzone + (10 * i * this.yardToPixelMultipler), (document.getElementById(this.elementId).height * this.yardMarkersSecondLineMultiplier));
+			this.ctx.fillText(str_yard_marker, int_width_of_endzone + (10 * i * this.yardToPixelMultipler), (document.getElementById(this.element_id).height * this.yardMarkersSecondLineMultiplier));
 		}
 	}
 	
  	this.drawLine = function( x ) {
 		this.ctx.beginPath();
 		this.ctx.moveTo(x, 0);
-		this.ctx.lineTo(x, document.getElementById(this.elementId).height);
+		this.ctx.lineTo(x, document.getElementById(this.element_id).height);
 		this.ctx.stroke();
 	}
 
@@ -126,10 +130,10 @@ function Field( elementId, visitorTeamAbbreviation, homeTeamAbbreviation ) {
 	
 	this.fillEndZones = function(){
 		this.ctx.fillStyle = "green";
-		this.ctx.fillRect(0, 0, (this.endZoneWidth * this.yardToPixelMultipler), document.getElementById(this.elementId).height);
+		this.ctx.fillRect(0, 0, (this.endZoneWidth * this.yardToPixelMultipler), document.getElementById(this.element_id).height);
 
 		this.ctx.fillStyle = "green";
-		this.ctx.fillRect(document.getElementById(this.elementId).width - (this.endZoneWidth * this.yardToPixelMultipler), 0, (this.endZoneWidth * this.yardToPixelMultipler), document.getElementById(this.elementId).height);
+		this.ctx.fillRect(document.getElementById(this.element_id).width - (this.endZoneWidth * this.yardToPixelMultipler), 0, (this.endZoneWidth * this.yardToPixelMultipler), document.getElementById(this.element_id).height);
 	}
 
 	this.getYardMarker = function( i ) {
@@ -255,5 +259,5 @@ function Field( elementId, visitorTeamAbbreviation, homeTeamAbbreviation ) {
 	*/
 
 	this.setTeamInfo();	
-	this.draw();
+	this.drawField();
 }
